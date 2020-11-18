3f113ca03c91c61a4d86914580a39a7e6f8f0a01
Aaron Cleveland <toscleveland@gmail.com>
2020-11-18 02:07:44 -0600

    Initial Commit

---
+++ SwiftUI-Barcode-Scanner/SwiftUI_Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Barcode_ScannerApp.swift
+//  SwiftUI-Barcode-Scanner
+//
+//  Created by 09299775 on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
