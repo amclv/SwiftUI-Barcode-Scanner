fb8bb666f2e597b073205ff450c1945349f6ad0b
amclv <toscleveland@gmail.com>
2021-06-08 00:57:19 -0500

    UI Setup, Camera UIViewController setup.

---
+++ SwiftUI-Barcode-Scanner/ScannerViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ScannerViewController.swift
+//  SwiftUI-Barcode-Scanner
+//
+//  Created by Aaron Cleveland on 6/8/21.
+//
+
+import UIKit
+import AVFoundation
+
+protocol ScannerViewDelegate: AnyObject {
+    func didFind(barcode: String)
+}
+
+final class ScannerViewController: UIViewController {
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerViewDelegate?
+    
+    init(scannerDelegate: ScannerViewDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            // .ean13 and .ean8 are barcodes
+            metaDataOutput.metadataObjectTypes = [.ean13, .ean8]
+        } else {
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer!.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let object = metadataObjects.first else {
+            return
+        }
+        
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+            return
+        }
+        
+        guard let barcode = machineReadableObject.stringValue else {
+            return
+        }
+        
+        scannerDelegate?.didFind(barcode: barcode)
+    }
+}
--- SwiftUI-Barcode-Scanner/SwiftUI_Barcode_ScannerApp.swift
+++ SwiftUI-Barcode-Scanner/SwiftUI_Barcode_ScannerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_Barcode_ScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
